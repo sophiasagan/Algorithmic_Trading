1a505e5d286b68389bc4f18b941f668a554cb391
Sophia Sagan <sophiabraddock82@gmail>
2020-08-20 23:02:40 -0400

    plotly error

--- Final/Strategies.py
+++ Final/Strategies.py
@@ -1,51 +1,51 @@
+class Strategies:
 
+	@staticmethod
+	def maStrategy(df, i:int):
+		''' If price is 10% below the Slow MA, return True'''
 
-@staticmethod
-def maStrategy(df, i:int):
-        # if price is 10% below the Slow MA, put a Buy Signal Return True
+		buy_price = 0.96 * df['slow_sma'][i]
+		if buy_price >= df['close'][i]:
+			return min(buy_price, df['high'][i])
 
-        buy_price = 0.9 * df['slow_sma'][i]
-        if buy_price >= df['close'][i]:
-            return min(buy_price, df['high'][i])
+		return False
 
-        return False
+	@staticmethod
+	def bollStrategy(df, i:int):
+		''' If price is 2.5% below the Lower Bollinger Band, return True'''
 
-@staticmethod
-def bollStrategy(df, i:int):
-    # if price is 5% below the Lower Bollinger Band, return True
+		buy_price = 0.975 * df['low_boll'][i]
+		if buy_price >= df['close'][i]:
+			return min(buy_price, df['high'][i])
 
-    buy_price = 0.975 * df['low_boll'][i]
-    if buy_price >= df['close'][i]:
-        return min(buy_price, df['high'][i])
+		return False
 
-    return False
+# def Main():
+#     # symbol = 'BTCUSDT'
+#     # model = TradingModel(symbol)
+#     # model.strategy()
+#     exchange = Binance()
 
-def Main():
-    # symbol = 'BTCUSDT'
-    # model = TradingModel(symbol)
-    # model.strategy()
-    exchange = Binance()
+#     symbols = exchange.getTradingSymbols()
 
-    symbols = exchange.getTradingSymbols()
+#     for symbol in symbols:
 
-    for symbol in symbols:
+#         print(symbol)
 
-        print(symbol)
+#         model = TradingModel(symbol)
 
-        model = TradingModel(symbol)
+#         plot = False
 
-        plot = False
+#         # if model.maStrategy(len(model.df['close'])-1):
+#         #     print(" MA Strategy match on " +symbol)
+#         #     plot = True
 
-        # if model.maStrategy(len(model.df['close'])-1):
-        #     print(" MA Strategy match on " +symbol)
-        #     plot = True
+#         if model.bollStrategy(len(model.df['close'])-1):
+#             print(" Boll Strategy match on " +symbol)
+#             plot = True
 
-        if model.bollStrategy(len(model.df['close'])-1):
-            print(" Boll Strategy match on " +symbol)
-            plot = True
+#         if plot:
+#             model.plotData()
 
-        if plot:
-            model.plotData()
-
-if __name__ == '__main__':
-    Main()
+# if __name__ == '__main__':
+#     Main()
