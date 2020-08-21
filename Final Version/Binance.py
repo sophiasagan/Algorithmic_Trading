17f3cd8d412fe0f8d4e663419be36cff18ca31a9
Sophia Sagan <sophiabraddock82@gmail>
2020-08-21 18:57:48 -0400

    working

--- Final Version/Binance.py
+++ Final Version/Binance.py
@@ -1,199 +1,224 @@
-import requests
+import requests 
 import json
 import decimal
 import hmac
 import time
 import pandas as pd
+import hashlib
 
 binance_keys = {
-    'api_key': "PASTE API KEY HERE",
-    'secret_key': 'PASTE SECRET KEY HERE'
+	'api_key': "key",
+	'secret_key': "key"
 }
 
 class Binance:
-    def __init__(self):
+	def __init__(self):
 
-        self.base = 'https://api.binance.com'
+		self.base = 'https://api.binance.com'
 
-        self.endpoints = {
-            "order": '/api/v1/order',
-            "testOrder": '/api/v1/order/test',
-            "allOrders": '/api/v1/allOrders',
-            "klines": '/api/v1/klines',
-            "exchangeInfo": '/api/v1/exchangeInfo'
-        }
+		self.endpoints = {
+			"order": '/api/v3/order',
+			"testOrder": '/api/v3/order/test',
+			"allOrders": '/api/v3/allOrders',
+			"klines": '/api/v3/klines',
+			"exchangeInfo": '/api/v3/exchangeInfo'
+		}
 
-    def getTradingSymbols(self):
-        # Gets all symbols which are tradable (currently)
-        url = self.base + self.endpoints['exchangeInfo']
+		self.headers = {"X-MBX-APIKEY": binance_keys['api_key']}
 
-        try:
-            response = requests.get(url)
-            data = json.loads(response.text)
-        except Exception as e:
-            print("Exception occured when trying to access "+url)
-            print(e)
-            return[]
+	def GetTradingSymbols(self, quoteAssets:list=None):
+		''' Gets All symbols which are tradable (currently) '''
+		url = self.base + self.endpoints["exchangeInfo"]
 
-        symbols_list = []
+		try: 
+			response = requests.get(url)
+			data = json.loads(response.text)
+		except Exception as e:
+			print(" Exception occured when trying to access "+url)
+			print(e)
+			return []
 
-        for pair in data['symbols']:
-            if pair['status'] == 'TRADING':
-                symbols_list.append(pair['symbol'])
+		symbols_list = []
 
-        return symbols_list
+		for pair in data['symbols']:
+			if pair['status'] == 'TRADING':
+				if quoteAssets != None and pair['quoteAsset'] in quoteAssets:
+					symbols_list.append(pair['symbol'])
 
-    def GetSymbolData(self, symbol:str, interval:str):
-        # Gets trading data for one symbol
-        params = '?&symbol='+symbol+'&interval='+interval # specify the sumbol that we're looking for ( BTC/USD ) and the length/interval of candlestick (i.e. 1hr)
+		return symbols_list
 
-        url = self.base + self.endpoints['klines'] + params
+	def GetSymbolData(self, symbol:str, interval:str):
+		''' 
+		Gets trading data for one symbol 
+		
+		Parameters
+		--
+			symbol str:        The symbol for which to get the trading data
+			interval str:      The interval on which to get the trading data
+				minutes      '1m' '3m' '5m' '15m' '30m'
+				hours        '1h' '2h' '4h' '6h' '8h' '12h'
+				days         '1d' '3d'
+				weeks        '1w'
+				months       '1M;
+		'''
 
-        # dowload data
-        data = requests.get(url)
-        dictionary = json.loads(data.text)
+		params = '?&symbol='+symbol+'&interval='+interval
 
-        # put in dataframe and clean-up
-        df = pd.DataFrame.from_dict(dictionary) # getting data
-        df = df.drop(range(6, 12), axis=1) # drop colums 6-12
+		url = self.base + self.endpoints['klines'] + params
 
-        # rename columns
-        col_names = ['time', 'open', 'high', 'low', 'close', 'volume'] # rename remaining columns
-        df.columns = col_names
+		# download data
+		data = requests.get(url)
+		dictionary = json.loads(data.text)
 
-        # transform values from strings to floats
-        for col in col_names:
-            df[col] = df[col].astype(float)
+		# put in dataframe and clean-up
+		df = pd.DataFrame.from_dict(dictionary)
+		df = df.drop(range(6, 12), axis=1)
 
-        
-        return(df)
+		# rename columns
+		col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
+		df.columns = col_names
 
-    def PlaceOrder(self, symbol:str, side:str, type:str, quantity:float, price:float, test:bool=True):
-        '''
-        Symbol: ETHBTC
+		# transform values from strings to floats
+		for col in col_names:
+			df[col] = df[col].astype(float)
 
-        ETH - base asset (what we buy)
-        BTC - quote asset (what we sell for)
-        quantity - how much ETH we want
-        price - how much BTC we're willing to sell it for
-        '''
-        
-        params = {
-            'symbol': symbol,
-            'side': side, # buy or sell
-            'type': type, # market, limit, stop loss etc
-            'timeInForce': 'GTC', 
-            'quantity': quantity,
-            'price': self.floatToString(price),
-            'recvWindow': 5000,
-            'timestamp': int(round(time.time()*1000))
-        }
+		df['date'] = pd.to_datetime(df['time'] * 1000000, infer_datetime_format=True)
 
-        self.signRequest(params)
+		return df
 
-        url = ''
-        if test:
-            url = self.base * self.endpoints['testOrder']
-        else:
-            url = self.base * self.endpoints['order']
 
-        try:
-            response = requests.post(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']}) # headers for account access
-            
-        except Exception as e:
-            print("Exception occured when trying to place order on "+url)
-            print(e)
-            response = {'code': '-1', 'msg':e}
-            return None
+	def PlaceOrder(self, symbol:str, side:str, type:str, quantity:float=0, price:float=0, test:bool=True):
 
-        return json.loads(response.text)
+		'''
+		Places an order on Binance
+		Parameters
+		--
+			symbol str:        The symbol for which to get the trading data
+			side str:          The side of the order 'BUY' or 'SELL'
+			type str:          The type, 'LIMIT', 'MARKET', 'STOP_LOSS'
+			quantity float:    .....
+		'''
 
-    def floatToString(self, f:float):
-        # converts the given float to a string, without resorting to the scientific notation
-        ctx = decimal.Context()
-        ctx.prec = 12
-        d1 = ctx.create_decimal(repr(f))
-        return format(d1, 'f')
+		params = {
+			'symbol': symbol,
+			'side': side, 			# BUY or SELL
+			'type': type,				# MARKET, LIMIT, STOP LOSS etc
+			'quantity': quantity,
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
 
-    def signRequest(self, params:dict):
-        # signs the request to the Binance API
-        query_string = '&'.join(["{}={}".format(d, params[d]) for d in params])
-        signature = hmac.new(binance_keys['secret_key'].encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
-        params['signature'] = signature.hexdigest()
+		if type != 'MARKET':
+			params['timeInForce'] = 'GTC'
+			params['price'] = self.floatToString(price)
 
-    def CancelOrder(self, symbol:str, orderId:str):
-        '''
-            Cancels the order on a symbol based on orderId
-        '''
-        
-        params = {
-            'symbol': symbol,
-            'orderId': orderId,
-            'recvWindow': 5000,
-            'timestamp': int(round(time.time()*1000))
-        }
+		self.signRequest(params)
 
-        self.signRequest(params)
+		url = ''
+		if test: 
+			url = self.base + self.endpoints['testOrder']
+		else:
+			url = self.base + self.endpoints['order']
 
-        url = self.base + self.endpoints['order']
-        
-        try:
-            response = requests.delete(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']}) # headers for account access
-            
-        except Exception as e:
-            print("Exception occured when trying to place order on "+url)
-            print(e)
-            response = {'code': '-1', 'msg':e}
-            return None
+		try: 
+			response = requests.post(url, params=params, headers=self.headers)
+			data = response.text
+		except Exception as e:
+			print(" Exception occured when trying to palce order on "+url)
+			print(e)
+			data = {'code': '-1', 'msg':e}
 
-    def GetOrderInfo(self, symbol:str, orderId:str):
-        '''
-            Gets the order on a symbol based on orderId
-        '''
-        
-        params = {
-            'symbol': symbol,
-            'orderId': orderId,
-            'recvWindow': 5000,
-            'timestamp': int(round(time.time()*1000))
-        }
+		return json.loads(data)
 
-        self.signRequest(params)
+	def CancelOrder(self, symbol:str, orderId:str):
+		'''
+			Cancels the order on a symbol based on orderId
+		'''
 
-        url = self.base + self.endpoints['order']
-        
-        try:
-            response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']}) # headers for account access
-            
-        except Exception as e:
-            print("Exception occured when trying to get order info on "+url)
-            print(e)
-            response = {'code': '-1', 'msg':e}
-            return None
+		params = {
+			'symbol': symbol,
+			'orderId' : orderId,
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
 
-        return json.loads(response.text)
+		self.signRequest(params)
 
-    def GetAllOrderInfo(self, symbol:str, orderId:str):
-        '''
-            Gets info about all order on a symbol
-        '''
-        
-        params = {
-            'symbol': symbol,
-            'timestamp': int(round(time.time()*1000))
-        }
+		url = self.base + self.endpoints['order']
 
-        self.signRequest(params)
+		try: 
+			response = requests.delete(url, params=params, headers=self.headers)
+			data = response.text
+		except Exception as e:
+			print("Exception occured when trying to cancel order on "+url)
+			print(e)
+			data = {'code': '-1', 'msg':e}
 
-        url = self.base + self.endpoints['allOrders']
-        
-        try:
-            response = requests.get(url, params=params, headers={"X-MBX-APIKEY": binance_keys['api_key']}) # headers for account access
-            
-        except Exception as e:
-            print("Exception occured when trying to get all orders on "+url)
-            print(e)
-            response = {'code': '-1', 'msg':e}
-            return None
+		return json.loads(data)
 
-        return json.loads(response.text)
+	def GetOrderInfo(self, symbol:str, orderId:str):
+		'''
+			Gets info about an order on a symbol based on orderId
+		'''
+
+		params = {
+			'symbol': symbol,
+			'orderId' : orderId,
+			'recvWindow': 5000,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = self.base + self.endpoints['order']
+
+		try: 
+			response = requests.get(url, params=params, headers=self.headers)
+			data = response.text
+		except Exception as e:
+			print(" Exception occured when trying to get order info on "+url)
+			print(e)
+			data = {'code': '-1', 'msg':e}
+
+		return json.loads(data)
+
+
+	def GetAllOrderInfo(self, symbol:str):
+		'''
+			Gets info about all order on a symbol
+		'''
+
+		params = {
+			'symbol': symbol,
+			'timestamp': int(round(time.time()*1000))
+		}
+
+		self.signRequest(params)
+
+		url = self.base + self.endpoints['allOrders']
+
+		try: 
+			response = requests.get(url, params=params, headers=self.headers)
+			data = response.text
+		except Exception as e:
+			print("Exception occured when trying to get info on all orders on "+url)
+			print(e)
+			data = {'code': '-1', 'msg':e}
+
+		return json.loads(data)
+
+	def floatToString(self, f:float):
+		''' Converts the given float to a string,
+		without resorting to the scientific notation '''
+
+		ctx = decimal.Context()
+		ctx.prec = 12
+		d1 = ctx.create_decimal(repr(f))
+		return format(d1, 'f')
+
+	def signRequest(self, params:dict):
+		''' Signs the request to the Binance API '''
+
+		query_string = '&'.join(["{}={}".format(d, params[d]) for d in params])
+		signature = hmac.new(binance_keys['secret_key'].encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
+		params['signature'] = signature.hexdigest()
